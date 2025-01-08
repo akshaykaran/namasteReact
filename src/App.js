5159107c6283dc6aa20cb7265a7f171e08c00c99
Akshay-Vnk <akshaykaran2@gmail.com>
2025-01-09 00:15:42 +0530

    deployent

--- src/App.js
+++ src/App.js
@@ -78,3 +78,5 @@ const appRouter = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<RouterProvider router={appRouter} />);
+
+export default AppLayout;
